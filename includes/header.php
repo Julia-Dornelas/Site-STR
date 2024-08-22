d7313a36a7310bd172152e68f86a77f8ad9c4886
Júlia Dornelas <129866827+Julia-Dornelas@users.noreply.github.com>
2024-08-22 16:59:05 -0300

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,48 @@
+<header>
+        <div class="pos-f-t border-bottom border-success ">
+          <div class="collapse" id="navbarToggleExternalContent">
+            <div class="p-4" style="background-color: #7db798;" >
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item text-white " style="background-color: #7db798;">
+                  <a href="index.html">Home</a>
+                </li>
+                <li class="list-group-item text-white " style="background-color: #7db798;">
+                  <a href="catalogo.g-darkhtml">Institucional</a>
+                </li>
+                <li class="list-group-item text-white " style="background-color: #7db798;">
+                  <a href="catalogocolecionador.html">Diretoria</a>
+                </li>
+                <li class="list-group-item text-white " style="background-color: #7db798;">
+                  <a  href="sobre.html">Serviços</a>
+                </li>
+                <li class="list-group-item text-white " style="background-color: #7db798;">
+                  <a  href="contatos.html">Supermercado</a>
+                </li>
+                <li class="list-group-item text-white " style="background-color: #7db798;">
+                    <a  href="contatos.html">Emissão de boletos</a>
+                </li>
+                 <li class="list-group-item text-white " style="background-color: #7db798;">
+                    <a  href="contatos.html">Links Uteis</a>
+                </li>
+              </ul>
+            </div>
+            
+          </div>
+    
+          <nav class="d-flex justify-content-center navbar navbar-dark" style="background-color: #7db798 ;">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent"
+              aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Alterna navegação">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+    
+            <!--Logo-->
+    
+            <img class="Logoheader rounded mx-auto" src="img/Logos/LogoSjCar.svg" alt="" />
+          </nav>
+          
+          <div class="progress">
+            <div class="progress-bar bg-success" role="progressbar" style="width: 100%" ></div>
+          </div>
+
+        </div>
+      </header>
---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Home</title>
+    
+</head>
+<body>
+    <?php include 'includes/header.php' ?>
+    
+<!--CARD 1-->
+
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src=".../100px180/?text=Image cap" alt="Imagem de capa do card">
+  <div class="card-body">
+    <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
+  </div>
+</div> 
+    
+<!--CARD 2-->
+
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src=".../100px180/?text=Image cap" alt="Imagem de capa do card">
+  <div class="card-body">
+    <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
+  </div>
+</div> 
+    
+<!--CARD 3-->
+
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src=".../100px180/?text=Image cap" alt="Imagem de capa do card">
+  <div class="card-body">
+    <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
+  </div>
+</div> 
+
+
+
+
+
+<?php include 'includes/js.php' ?>
+
+</body>
+</html>
